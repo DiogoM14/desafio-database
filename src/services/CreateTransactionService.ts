59d95c64438c23ff5fdf9b2f2a965485fa1f23be
Xavi002 <diogomartins200214@gmail.com>
2020-07-16 22:39:03 +0100

    delete transactions and added categories

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,7 +1,12 @@
 // import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+
+import { getCustomRepository, getRepository } from 'typeorm';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
+
 import Transaction from '../models/Transaction';
+
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -18,11 +23,27 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category); // Cria um repositório a partir da model
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
 
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionsRepository.save(transaction);
